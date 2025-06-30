8327a4a9bc94bbe67ad757d7db7233f1377daa0b
the-tunnster <therealtarunkrishnan@gmail.com>
2025-06-30 14:34:27 +1000

    Refactor update functions to use URL constants and improve error handling

--- database/update.py
+++ database/update.py
@@ -1,6 +1,8 @@
 import requests
-import json
+from typing import Any
 from dataclasses import asdict
+
+from config.urls import *
 
 from models.user import User
 from models.routines import FullRoutine
@@ -8,15 +10,16 @@ from models.routines import FullRoutine
 def updateUserProfile(user: User, user_id: str) -> bool:
     try:
         response = requests.patch(
-            url=f"http://localhost:8080/user/update?user_id={user_id}",
-            headers={"Content-Type": "application/json"},
-            data=json.dumps(asdict(user))
+            url=USER_URLS["update"],
+            params={"user_id": user_id},
+            json=asdict(user)
         )
 
-        if response.status_code in (200, 204):  # 204 = No Content, 200 = OK
+        if response.status_code in (200, 204):
+            print(f"User profile for ID {user_id} updated successfully.")
             return True
         else:
-            print(f"Error: {response.status_code}, {response.text}")
+            print(f"Error updating user profile: Status Code {response.status_code}, Response: {response.text}")
             return False
 
     except Exception as e:
@@ -26,16 +29,22 @@ def updateUserProfile(user: User, user_id: str) -> bool:
 
 def updateUserRoutine(user: User, routine: FullRoutine) -> bool:
     try:
+        payload: dict[str, Any] = {
+            "name": routine.name,
+            "exercises": [asdict(ex) for ex in routine.exercises]
+        }
+
         response = requests.patch(
-            url=f"http://localhost:8080/routines/update?user_id={user.id}&routine_id={routine.id}",
-            headers={"Content-Type": "application/json"},
-            data=json.dumps(asdict(routine))
+            url=ROUTINE_URLS["update"],
+            params={"user_id": user.id, "routine_id":routine.id},
+            json=payload
         )
 
         if response.status_code in (200, 204):  # 204 = No Content, 200 = OK
+            print(f"User routine for ID {routine.id} updated successfully.")
             return True
         else:
-            print(f"Error: {response.status_code}, {response.text}")
+            print(f"Error updating user routine: Status Code {response.status_code}, Response: {response.text}")
             return False
 
     except Exception as e:
