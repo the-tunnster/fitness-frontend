07cd6201b266910515b72ee33d2ee4160a023ba2
the-tunnster <therealtarunkrishnan@gmail.com>
2025-07-03 00:48:10 +1000

    Added a session deletion handler

--- database/delete.py
+++ database/delete.py
@@ -1,15 +1,29 @@
 import requests
 
+from config.urls import *
 from models.user import *
 from models.routines import *
 from models.exercise import *
 
 def deleteRoutine(user_id:str, routine_id: str) -> bool :
     try:
-        response = requests.request(
-            method="GET",
-            url="http://localhost:8080/routines/delete",
+        response = requests.delete(
+            url=ROUTINE_URLS["delete"],
             params={"user_id":user_id, "routine_id": routine_id}
+        )
+
+        if response.status_code == 204 :
+            return True
+    except:
+        return False
+	
+    return False
+
+def deleteSession(session_id: str) -> bool :
+    try:
+        response = requests.delete(
+            url=SESSION_URLS["delete"],
+            params={"session_id":session_id}
         )
 
         if response.status_code == 204 :
