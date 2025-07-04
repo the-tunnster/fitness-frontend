5ba3263afcc46cc3f2fbaa12f014fcd11e49d4c1
the-tunnster <therealtarunkrishnan@gmail.com>
2025-07-04 12:42:38 +1000

    Implemented: dataclass for new history objects

--- models/exercise.py
+++ models/exercise.py
@@ -11,3 +11,22 @@ class Exercise:
     variations: List[str]
     equipment: List[str]
     id: str
+
+@dataclass
+class WorkoutSet:
+    reps: int
+    weight: float
+
+@dataclass
+class ExerciseSets:
+    date: str
+    equipment: str
+    variation: str
+    sets: List[WorkoutSet]
+
+@dataclass
+class ExerciseHistory:
+    id: str
+    user_id: str
+    exercise_id: str
+    exercise_sets: List[ExerciseSets]
