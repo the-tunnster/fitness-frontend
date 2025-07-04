c22ecc8f1e1991ff9c303b845df33b2911e12912
the-tunnster <therealtarunkrishnan@gmail.com>
2025-07-04 12:44:20 +1000

    Work in progress type shii

--- pages/analytics.py
+++ pages/analytics.py
@@ -22,7 +22,13 @@ user_data: User
 
 if streamlit.session_state["user_data"] is None:
     streamlit.session_state["user_data"] = getUser(str(streamlit.user.email))
+user_data = streamlit.session_state["user_data"]
     
+streamlit.header("Analytics")
+
+streamlit.write("Work in progress type shii")
+streamlit.write("May take a week or so, my bad g")
+
 # Check analytics collection for consolidated workout history
 # 	If yes --> fetch all exercise data
 # 	If no  --> build analytics document
