e78e76e9dde8cfdf4a475f2175415397b5d151e0
the-tunnster <therealtarunkrishnan@gmail.com>
2025-07-12 18:58:44 +1000

    Update feature descriptions to use markdown for better formatting

--- home.py
+++ home.py
@@ -45,13 +45,13 @@ streamlit.header("Welcome " + streamlit.session_state["user_data"].username + "!
 streamlit.write("This application is designed to help you track your fitness journey, manage routines, record workouts, and analyze progress.")
 
 streamlit.subheader("Features Overview", anchor=False)
-streamlit.write("""
+streamlit.markdown("""
 - **Workout Recorder**: Log your cardio and strength workouts.
 - **Routine Manager**: Create and manage your personal routines.
 - **Analytics**: Dive into your workout data with historic and post-workout analytics.
 - **User Profile**: Update and manage your personal information.
 
-Feel free to explore and make the most of the features, and I'll be adding new ones as I go.
-In the meantime, if you encounter any issues or think of any cool new features you want, let me know and I'll get on it.
+Feel free to explore and make the most of the features, and I'll be adding new ones as I go. </br>
+In the meantime, if you encounter any issues or think of any cool new features you want, let me know and I'll get on it. </br>
 Good luck!
-""")
+""", unsafe_allow_html=True)
