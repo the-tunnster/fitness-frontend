71fe6475dbe957405dc79373ba86eb20801277bd
the-tunnster <therealtarunkrishnan@gmail.com>
2025-07-16 16:40:04 +1000

    Add post-cardio analysis page with initial setup and user authentication

---
+++ pages/analytics/post-cardio.py
@@ -0,0 +1,20 @@
+import streamlit
+
+from helpers.cache_manager import *
+from helpers.user_interface import *
+
+from database.read import *
+
+streamlit.set_page_config(
+    page_title="Post Cardio Analysis",
+    page_icon=":material/table_chart_view:",
+    layout="wide",
+)
+
+if not streamlit.user.is_logged_in:
+    streamlit.switch_page("home.py")
+
+uiSetup()
+initSessionState(["user_data", ])
+
+streamlit.header("Coming Soon...", anchor=False)
