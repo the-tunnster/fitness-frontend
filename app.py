44d2e73164942dbfae97f48f0a4322540b6d4080
the-tunnster <therealtarunkrishnan@gmail.com>
2025-07-21 15:22:53 +1000

    Refactor import statements and reorder function calls for improved clarity in app initialization

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ import plotly.graph_objects as go
 
 from helpers.user_interface import *
 
+from database.read import getExerciseList, getCardioList
+
 streamlit.set_page_config(layout="wide")
+
+getCardioList()
+getExerciseList()
+
 setupNavigation()
