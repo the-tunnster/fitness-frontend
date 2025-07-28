84ca33114c62e71c3abb92383f1085386667a902
the-tunnster <therealtarunkrishnan@gmail.com>
2025-07-28 19:16:56 +1000

    Remove muscle attributes from Exercise class to simplify data model

--- models/exercise.py
+++ models/exercise.py
@@ -5,9 +5,6 @@ from typing import List
 class Exercise:
     name: str
     category: str
-    primary_muscle: str
-    secondary_muscle: str
-    tertiary_muscle: str
     variations: List[str]
     equipment: List[str]
     id: str
