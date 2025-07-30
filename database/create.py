05edafd23ce4c2134d0e35c54b4228cb282e3ba4
the-tunnster <therealtarunkrishnan@gmail.com>
2025-07-30 15:01:22 +1000

    Update clearance level for dummy user profile from 0 to 1

--- database/create.py
+++ database/create.py
@@ -17,7 +17,7 @@ def createDummyUserProfile(emailID: str, username: str) -> bool:
         height=175.0,
         weight=78.5,
         unitPreference="metric",
-        clearanceLevel=0,
+        clearanceLevel=1,
         id=None
     )
 
