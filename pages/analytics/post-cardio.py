58be95feb03219f94589a2ee7c8c6d8d50fae933
the-tunnster <therealtarunkrishnan@gmail.com>
2025-08-13 11:05:15 +1000

    Refactor cardio history and recorder pages to develop strava interation (later)

--- pages/analytics/post-cardio.py
+++ pages/analytics/post-cardio.py
@@ -1,5 +1,4 @@
-import streamlit
-import plotly.graph_objects as go                                                                                   # type:ignore
+import streamlit                                                                     # type:ignore
 
 from helpers.cache_manager import *
 from helpers.user_interface import *
@@ -11,7 +10,7 @@ if not streamlit.user.is_logged_in:
     streamlit.switch_page("home.py")
 
 uiSetup()
-initSessionState(["user_data", ])
+initSessionState(["user_data", "strava_code"])
 
 streamlit.title("Cardio History", anchor=False)
 
@@ -20,102 +19,4 @@ if streamlit.session_state["user_data"] is None:
     streamlit.session_state["user_data"] = getUser(str(streamlit.user.email))
 user_data = streamlit.session_state["user_data"]
 
-if user_data.clearanceLevel < 2:
-    streamlit.switch_page("home.py")
-
-cardio_list = getCardioList()
-cardio_names: list[str]
-
-if cardio_list is not None:
-    cardio_names = ["None"] + [cardio.name for cardio in cardio_list]
-else:
-    streamlit.stop()
-
-selected_cardio_name = streamlit.selectbox(
-    label="Select an exercise to view its history:",
-    options=cardio_names,
-    key="select_exercise_for_analytics",
-    index=0
-)
-
-if selected_cardio_name == "None":
-    streamlit.stop()
-
-selected_cardio_data = cardio_list[cardio_names.index(selected_cardio_name) - 1]
-
-data: list[dict[str, Any]] | None = getCardioHistoryData(user_data.id, selected_cardio_data.id)
-if data is None:
-    streamlit.info("You don't have enough workouts to display any useful data. Hit the gym bruv.")
-    streamlit.stop()
-
-fig = go.Figure()
-
-# Extracting data for plotting
-dates = [session['date'] for session in data['sessions']]                                                            # type:ignore
-total_distances = [session['metrics']['total_distance'] for session in data['sessions']]                             # type:ignore
-total_times = [session['metrics']['total_time'] for session in data['sessions']]                                     # type:ignore
-
-# Total Distance (Primary Axis)                                                     
-fig.add_trace(go.Scatter(                                                                                            # type:ignore
-    x=dates,
-    y=total_distances,
-    mode='lines',
-    name='Total Distance',
-    line=dict(color='rgba(255, 145, 164, 1)', width=3, shape='spline'),
-    hovertemplate='%{x|%b %d, %Y}<br>Distance: %{y:.0f} m', # Assuming meters
-))
-
-# Total Time (Secondary Axis)
-fig.add_trace(go.Scatter(                                                                                            # type:ignore
-    x=dates,
-    y=total_times,
-    mode='lines',
-    name='Total Time',
-    yaxis='y2', # Assign to the secondary y-axis
-    line=dict(color='rgba(0, 128, 128, 1)', width=3, shape='spline'),
-    hovertemplate='%{x|%b %d, %Y}<br>Time: %{y:.0f} sec', # Assuming seconds
-))
-
-fig.update_layout(                                                                                                   # type:ignore
-    xaxis=dict(
-        fixedrange=True
-    ),
-    yaxis=dict(
-        fixedrange=True,
-        dtick=None,
-        gridcolor='rgba(0,0,0,0.1)',
-        ticks='inside',
-        tickfont=dict(size=10),
-        tickmode='auto',
-        nticks=5,
-    ),
-    yaxis2=dict(
-        overlaying='y',
-        side='left',
-        showgrid=False,
-        fixedrange=True,
-        dtick=None,
-        ticks='inside',
-        tickcolor='rgba(0, 0, 0, 1)',
-        tickfont=dict(size=10),
-        anchor='free',
-        position=1,
-        tickmode='auto',
-        nticks=5,
-    ),
-    template='simple_white',
-    legend=dict(
-        orientation="h",
-        yanchor="bottom",
-        y=1.1,
-        xanchor="center",
-        x=0.5,
-        itemsizing="constant",
-        itemwidth=30,
-        font=dict(size=12),
-    ),
-    margin=dict(l=0, r=0, t=0, b=0),
-)
-
-# Display the chart in Streamlit
-streamlit.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})                              # type:ignore
+streamlit.info("Sorry guys, gotta re-write this part. Should be out soon.")
--- pages/recorder/cardio.py
+++ pages/recorder/cardio.py
@@ -1,13 +1,11 @@
 import streamlit
 
-from database.read import getCardioList, getUser
-from database.update import updateCardioHistory
+from database.read import getUser
 
 from helpers.cache_manager import *
 from helpers.user_interface import *
 
 from models.user import User
-from models.cardio import CardioMetric, CardioSession
 
 
 if not streamlit.user.is_logged_in:
@@ -24,64 +22,4 @@ if streamlit.session_state["user_data"] is None:
     streamlit.session_state["user_data"] = getUser(str(streamlit.user.email))
 user_data = streamlit.session_state["user_data"]
 
-if user_data.clearanceLevel < 2:
-    streamlit.switch_page("home.py")
-
-cardio_list = getCardioList()
-cardio_names: list[str]
-
-if cardio_list is not None:
-    cardio_names = ["None"] + [cardio.name for cardio in cardio_list]
-else:
-    streamlit.stop()
-
-selected_cardio_name = streamlit.selectbox(
-    label="Select cardio",
-    options=cardio_names,
-    label_visibility="collapsed",
-)
-
-if selected_cardio_name == "None":
-    streamlit.stop()
-
-selected_cardio_data = cardio_list[cardio_names.index(selected_cardio_name) - 1]
-
-with streamlit.form("cardio_form", enter_to_submit=False):
-    col1, col2 = streamlit.columns([1, 1])
-
-    col1.write("Variation")
-    col1.text('')
-    variation = col2.selectbox("variation", options=selected_cardio_data.variations, label_visibility="collapsed")
-
-    col1.write("Total Distance")
-    col1.text('')
-    total_distance = col2.number_input("Enter total distance (km)", min_value=0.0, step=0.1, label_visibility="collapsed")
-
-    col1.write("Total Time")
-    col1.text('')
-    total_time = col2.number_input("Enter total time (minutes)", min_value=0.0, step=1.0, label_visibility="collapsed")
-
-    col1.write("Calories Burned")
-    col1.text('')
-    calories_burned = col2.number_input("Enter calories burned", min_value=0, step=1, label_visibility="collapsed")
-
-    col1.write("Heart Rate")
-    col1.text('')
-    heart_rate = col2.number_input("Enter heart rate (bpm)", min_value=0, step=1, label_visibility="collapsed")
-
-    submitted = streamlit.form_submit_button("Save Session Data", use_container_width=True)
-
-if submitted:
-    cardio_session = CardioSession(
-        variation=variation,
-        metrics=CardioMetric(
-            total_distance=total_distance,
-            total_time=total_time,
-            calories_burned=calories_burned,
-            heart_rate=heart_rate,
-        )
-    )
-
-    result = updateCardioHistory(user_data.id, selected_cardio_data.id, cardio_session)
-    if result:
-        streamlit.switch_page("pages/analytics/post-cardio.py")
+streamlit.info("Sorry guys, gotta re-write this part. Should be out soon.")
