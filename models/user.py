542d4b24422ccd752735704d609ee16b0d42674a
the-tunnster <therealtarunkrishnan@gmail.com>
2025-08-14 16:03:08 +1000

    Refactor user model classes to enhance structure and clarity

--- models/user.py
+++ models/user.py
@@ -1,13 +1,21 @@
 from dataclasses import dataclass
 
 @dataclass
-class User:
-    username: str
+class FullUser:
+    id: str
     email: str
     gender: str
-    dateOfBirth: str
+    username: str
     height: float
     weight: float
+    dateOfBirth: str
     unitPreference: str
     clearanceLevel: int
+    stravaAccessToken: str
+    stravaRefreshToken: str
+
+@dataclass
+class BasicUser:
     id: str
+    username: str
+    clearanceLevel: int
